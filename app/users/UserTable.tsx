f428d7495f3bc1c48f4b43b9e0b8c930973b52cc
Richard Harrison <r.harr.k@gmail.com>
2024-10-11 12:00:17 +0100

    allows users table to be sorted by clicking name column

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,6 @@
+"use client"
 import React from "react";
+import { sort } from "fast-sort";
 
 interface User {
   id: number;
@@ -6,27 +8,41 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-  const users: User[] = await res.json();
+const UserTable = () => {
+  const [users, setUsers] = React.useState<User[]>([]);
+
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+        cache: "no-store",
+      });
+      const fetchedUsers: User[] = await res.json();
+      setUsers(fetchedUsers);
+    };
+    fetchUsers();
+  }, []);
+
+  const sortByName = () => {
+    setUsers(sort(users).asc(user => user.name));
+  };
 
   return (
     <table className="table table-zebra">
       <thead>
         <tr>
-          <th>Name</th>
+          <th onClick={() => sortByName()}>Name</th>
           <th>Email</th>
         </tr>
       </thead>
-      <tbody></tbody>
-      {users.map((user) => (
-        <tr key={user.id}>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-        </tr>
-      ))}
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
