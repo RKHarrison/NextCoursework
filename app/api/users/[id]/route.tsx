75a06b9efa1c8d4827e4ab452c5432bbcfe89c86
Richard Harrison <r.harr.k@gmail.com>
2024-10-12 14:04:05 +0100

    PUT users/id request, moking validation of expising user and validating body

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -4,6 +4,19 @@ export function GET(
   request: NextRequest,
   { params: { id } }: { params: { id: number } }
 ) {
-  if (id > 10) return NextResponse.json({error: 'User not found'}, {status: 404});
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id, name: `User ${id}` });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params: { id } }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  if (id > 10 || id < 1)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ id, name: body.name });
 }
