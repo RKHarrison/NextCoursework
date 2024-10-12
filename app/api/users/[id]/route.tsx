9dc81a39de582b10574db8cf2672bcfa22094156
Richard Harrison <r.harr.k@gmail.com>
2024-10-12 14:34:38 +0100

    refactors request validation for PUT and POST users, utilisisng the ZOD validation library

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export function GET(
   request: NextRequest,
@@ -14,8 +15,9 @@ export async function PUT(
   { params: { id } }: { params: { id: number } }
 ) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   if (id > 10 || id < 1)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id, name: body.name });
