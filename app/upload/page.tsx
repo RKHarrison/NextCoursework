1595aa9c354d8ca9c0a3c76dfcfa4562f2d3fca8
Richard Harrison <r.harr.k@gmail.com>
2024-10-13 18:12:51 +0100

    Creates state to store cloud publicId of image on successful upload and render the image using CldImage component

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,13 +1,39 @@
-'use client'
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+"use client";
+import React, { useState } from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="NextAppUploadPreset">
-      {({ open }) => <button className="btn btn-primary" onClick={() => open()
-      }>Upload</button>}
-    </CldUploadWidget>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width="300"
+          height="300"
+          alt="Uploaded image"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="NextAppUploadPreset"
+        onSuccess={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
