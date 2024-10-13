04d0828a02ffe3d1582fb595ce106124da9ff70f
Richard Harrison <r.harr.k@gmail.com>
2024-10-13 16:19:31 +0100

    refactors GET product endpoint to retrieve products from local mysql db via pisma

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Toothbrush", price: 599 },
-    { id: 2, name: "Toothpaste", price: 199 },
-    { id: 3, name: "Mouthwash", price: 299 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
