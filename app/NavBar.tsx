7df131d47fd504c1229f55546d89119bb853ffc6
Richard Harrison <r.harr.k@gmail.com>
2024-10-13 19:43:08 +0100

    moves session loading check into navbar to prevent whole navbar waiting for use laoding staus and renders a spinner

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const NavBar = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+
 
   return (
     <div className="flex bg-slate-200 p-5 space-x-10">
@@ -16,6 +16,7 @@ const NavBar = () => {
       <Link href="/users" className="mr-5">
         Users
       </Link>
+      {status === "loading" &&  <span className="loading loading-dots loading-sm"></span>}
       {status === "authenticated" && <div>{session.user!.name}</div>} 
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin" className="mr-5">
