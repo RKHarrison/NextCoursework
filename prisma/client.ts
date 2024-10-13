54eb1e18635d770519f440a65c34301eb9c832a7
Richard Harrison <r.harr.k@gmail.com>
2024-10-13 14:59:21 +0100

    refactors for clarity

--- prisma/client.ts
+++ prisma/client.ts
@@ -3,13 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prismaClientSingleton = () => new PrismaClient();
 
 declare const globalThis: {
-    prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") {
-    globalThis.prismaGlobal = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
