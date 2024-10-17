9990181c64d8a7baf26b6836c2e0c9932eca1f52
Richard Harrison <r.harr.k@gmail.com>
2024-10-17 21:59:07 +0100

    adds link to change password to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,17 @@
-'use client'
-import React from 'react'
-import { useSession } from 'next-auth/react'
+"use client";
+import React from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const DashboardPage = () => {
-const { status, data: session } = useSession();
+  const { status, data: session } = useSession();
 
   return (
-    <div> {session?.user && `${session.user.name}'s Dashboard`} </div>
-  )
-}
+    <div className='flex flex-col'>
+      <div> {session?.user && `${session.user.name}'s Dashboard`} </div>
+      <Link href="/dashboard/change-password" className="link link-primary link-hover">Change Password</Link>
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
