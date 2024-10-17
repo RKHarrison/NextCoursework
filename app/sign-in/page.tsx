003a3f983d6cde35880f72dcf140d15965754aca
Richard Harrison <r.harr.k@gmail.com>
2024-10-17 16:50:20 +0100

    adds title and styling to Sign In Page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,7 +3,10 @@ import SignInForm from "./SignInForm";
 
 const SignInPage = () => {
   return (
-    <div className="flex-initial px-20">
+    <div className=" px-20 pb-5 rounded-lg bg-slate-100">
+      <div className="text-4xl font-bold text-center my-10 ">
+        Please Sign In
+      </div>
       <SignInForm />
     </div>
   );
