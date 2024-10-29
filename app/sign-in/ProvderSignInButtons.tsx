dcb872efe10016587caeb26ed64898e468c8fa2b
Richard Harrison <r.harr.k@gmail.com>
2024-10-29 16:08:10 +0000

    adds images and styling to provide sign in buttons

--- app/sign-in/ProvderSignInButtons.tsx
+++ app/sign-in/ProvderSignInButtons.tsx
@@ -1,20 +1,34 @@
 "use client";
 import React from "react";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 const ProvderSignInButtons = () => {
   return (
-    <div className="join join-vertical space-y-5 items-center">
-      <button 
-        className="btn btn-primary btn-md min-w-52"
+    <div className="join join-vertical space-y-5 items-center  ">
+      <div className="divider">or</div>
+      <button
+        className="btn btn-outline btn-md min-w-52"
         onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
       >
+        <Image
+          src="/icons8-google-48.png"
+          alt="GitHub logo"
+          width={32}
+          height={32}
+        />
         Sign in with Google
       </button>
       <button
-        className="btn btn-primary btn-md min-w-52"
+        className="btn btn-outline btn-md min-w-52"
         onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
       >
+        <Image
+          src="/icons8-github-48.png"
+          alt="GitHub logo"
+          width={32}
+          height={32}
+        />
         Sign in with GitHub
       </button>
     </div>
