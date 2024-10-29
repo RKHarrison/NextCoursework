723bec216c0a9f173c8c0cc968ce4b0b6549c9ea
Richard Harrison <r.harr.k@gmail.com>
2024-10-29 16:12:47 +0000

    renames header with semantic html and restyles and words for consistency

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,9 +5,7 @@ import ProvderSignInButtons from "./ProvderSignInButtons";
 const SignInPage = () => {
   return (
     <div className="form-control space-y-5 items-center px-20 pb-5 rounded-lg bg-slate-100">
-      <div className="text-4xl font-bold text-center my-10 ">
-        Please Sign In
-      </div>
+      <h1 className="text-4xl font-bold text-center mt-5 ">Sign In</h1>
       <SignInForm />
       <ProvderSignInButtons />
     </div>
