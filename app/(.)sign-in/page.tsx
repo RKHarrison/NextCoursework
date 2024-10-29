ec69b586d348f1329eea642f27539cc159c3c866
Richard Harrison <r.harr.k@gmail.com>
2024-10-29 16:14:17 +0000

    adds new provider sign in buttons to modal interceptor too

--- app/(.)sign-in/page.tsx
+++ app/(.)sign-in/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Modal from "@/app/components/Modal";
 import SignInForm from "@/app/sign-in/SignInForm";
+import ProvderSignInButtons from "../sign-in/ProvderSignInButtons";
 
 const SignInModal = () => {
   return (
     <Modal title="Sign in">
-      <SignInForm/>
+      <SignInForm />
+      <ProvderSignInButtons />
     </Modal>
   );
 };
