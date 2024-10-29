5c7897d2799fcdae234a82ea12e01a9c750123d1
Richard Harrison <r.harr.k@gmail.com>
2024-10-29 16:22:03 +0000

    siplifies styling

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,7 +4,7 @@ import ProvderSignInButtons from "./ProvderSignInButtons";
 
 const SignInPage = () => {
   return (
-    <div className="form-control space-y-5 items-center px-20 pb-5 rounded-lg bg-slate-100">
+    <div className="form-control space-y-5 items-center">
       <h1 className="text-4xl font-bold text-center mt-5 ">Sign In</h1>
       <SignInForm />
       <ProvderSignInButtons />
