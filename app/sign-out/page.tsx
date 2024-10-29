2410927769ad104d08cf57fdb006489fefc5d9ab
Richard Harrison <r.harr.k@gmail.com>
2024-10-29 17:27:39 +0000

    redirects on sign out

--- app/sign-out/page.tsx
+++ app/sign-out/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { signOut } from "next-auth/react";
 
@@ -6,7 +6,12 @@ const SignOutPage = () => {
   return (
     <div className="join join-vertical space-y-5 mt-10">
       <h2>Are you sure you want to sign out?</h2>
-      <button className="btn btn-outline"onClick={() => signOut()}>Yes, sign out</button>
+      <button
+        className="btn btn-outline"
+        onClick={() => signOut({ callbackUrl: "/", redirect: true })}
+      >
+        Yes, sign out
+      </button>
     </div>
   );
 };
