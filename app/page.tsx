225cd53bdedf5d2173a8e9f8662568b4d4c8749c
Richard Harrison <r.harr.k@gmail.com>
2024-10-30 16:49:59 +0000

    adds bit.ly domain to image object in next.config in order to authorize the use of the domain for renderingimages, and demonstrates how to render with a fixed size

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
-import mountain from "@/public/mountain.jpg"
+import React from "react";
+import Image from "next/image";
+import mountain from "@/public/mountain.jpg";
 
 const Home = () => {
   return (
-    <Image src={mountain} alt="a sillouhette of a man standing on a mountain beneath a blue sky"/>
-  )
-}
+    <Image
+      src="https://bit.ly/react-cover"
+      alt="a sillouhette of a man standing on a mountain beneath a blue sky"
+      width={500}
+      height={500}
+    />
+  );
+};
 
-export default Home
+export default Home;
