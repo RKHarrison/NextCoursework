8996572ef9e5c4f4129c17e0978fdf4c6297ee1f
Richard Harrison <r.harr.k@gmail.com>
2024-10-30 17:12:54 +0000

    adresses error caused by main object requiring size properties

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import Image from "next/image";
 import mountain from "@/public/mountain.jpg";
 
+
 const Home = () => {
   return (
-    <Image
-      src="https://bit.ly/react-cover"
-      alt="a sillouhette of a man standing on a mountain beneath a blue sky"
-      fill
-      className="object-cover"
-      sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-      quality={100}
-      priority
-    />
+    <main className="relative w-screen h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="a sillouhette of a man standing on a mountain beneath a blue sky"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
+    </main>
   );
 };
 
