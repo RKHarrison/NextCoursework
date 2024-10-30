1121f3d4d21f4318ce6c9a3d90582ffdb216fb4d
Richard Harrison <r.harr.k@gmail.com>
2024-10-30 15:45:43 +0000

    adds email styling using tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -3,26 +3,29 @@ import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
 } from "@react-email/components";
 
 const WelcomeTemmplate = () => {
-    return (
-        <Html>
-            <Preview>Welcome aboard!</Preview>
-            <Body>
-                <Container>
-                    <Text>
-                        Welcome aboard! We're excited to have you as a new member of our community.
-                    </Text>
-                    <Link href='https://example.com/sign-in'>Sign in</Link>
-                </Container>
-            </Body>
-        </Html>
-    )
-
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Tailwind>
+        <Body className="bg-slate-200">
+          <Container>
+            <Text className="text-lg">
+              Welcome aboard! We&apos;re excited to have you as a new member of our
+              community.
+            </Text>
+            <Link className="text-2xl text-cyan-700" href="https://example.com/sign-in">Sign in</Link>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 };
 
 export default WelcomeTemmplate;
