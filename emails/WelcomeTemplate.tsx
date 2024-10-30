5ba0135eae55b96f4ec26cd821021017feb35c76
Richard Harrison <r.harr.k@gmail.com>
2024-10-30 15:46:37 +0000

    formats

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -17,10 +17,15 @@ const WelcomeTemmplate = () => {
         <Body className="bg-slate-200">
           <Container>
             <Text className="text-lg">
-              Welcome aboard! We&apos;re excited to have you as a new member of our
-              community.
+              Welcome aboard! We&apos;re excited to have you as a new member of
+              our community.
             </Text>
-            <Link className="text-2xl text-cyan-700" href="https://example.com/sign-in">Sign in</Link>
+            <Link
+              className="text-2xl text-cyan-700"
+              href="https://example.com/sign-in"
+            >
+              Sign in
+            </Link>
           </Container>
         </Body>
       </Tailwind>
