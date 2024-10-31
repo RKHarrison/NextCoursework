84b86fdfa925de83a371add044fb287f26a2ef79
Richard Harrison <r.harr.k@gmail.com>
2024-10-31 15:43:34 +0000

    assigns poppins font a variable name and uses it to provide the font to tailwind in tailwing.config so that it can be used for inline styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Roboto } from "next/font/google";
+import localFont from "next/font/local";
 import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
 import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 
 const inter = Inter({ subsets: ["latin"] });
-const roboto = Roboto({subsets: ['latin'], weight: ['400', '500', '700']});
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const poppins = localFont({
+  src: '../public/fonts/poppins-regular-webfont.woff2',
+  variable: '--font-poppins',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <GoogleAnalyticsScript />
-      <body className={roboto.className}>
+      <body className={poppins.variable}>
         <AuthProvider>
           <NavBar />
           <main className="p-5 flex min-w-full justify-center">{children}</main>
